b48b53df25ddda1ff6f6029019bf477500b75dd2
Sharon Mwende <sharonndisya2002@gmail.com>
2024-04-15 13:59:37 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -1,9 +1,22 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.DECIMAL(precision=10, scale=2))
+
+    def __repr__(self):
+        return f'<Plant {self.name}>'
